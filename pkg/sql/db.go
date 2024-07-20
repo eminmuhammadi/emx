416613cbe2e61d75333fe098a4a43ebd76c74d8a
Emin Muhammadi <emin.muhammadi@outlook.com>
2024-07-20 12:57:30 +0400

    Compliance fix

--- pkg/sql/db.go
+++ pkg/sql/db.go
@@ -15,64 +15,6 @@ limitations under the License.
 */
 package sql
 
-// Copyright (C) 1992-2020 Free Software Foundation, Inc.
-// This file is part of the GNU C Library.
-//
-// The GNU C Library is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public
-// License as published by the Free Software Foundation; either
-// version 2.1 of the License, or (at your option) any later version.
-//
-// The GNU C Library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public
-// License along with the GNU C Library; if not, see
-// <https://www.gnu.org/licenses/>.
-//
-// SPDX-License-Identifier: GPL-2.0+ WITH Linux-syscall-note
-// netinet/in/in_linux_ppc64le.go
-// signal/signal_linux_ppc64le.go
-// sys/socket/socket_linux_ppc64le.go
-//
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation; either version
-// 2 of the License, or (at your option) any later version.
-//
-// Copyright (C) 1992-2021 Free Software Foundation, Inc.
-//
-// This file is part of GCC.
-//
-// GCC is free software; you can redistribute it and/or modify it under
-// the terms of the GNU General Public License as published by the Free
-// Software Foundation; either version 3, or (at your option) any later
-// version.
-//
-// GCC is distributed in the hope that it will be useful, but WITHOUT ANY
-// WARRANTY; without even the implied warranty of MERCHANTABILITY or
-// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-// for more details.
-//
-// Under Section 7 of GPL version 3, you are granted additional
-// permissions described in the GCC Runtime Library Exception, version
-// 3.1, as published by the Free Software Foundation.
-//
-// You should have received a copy of the GNU General Public License and
-// a copy of the GCC Runtime Library Exception along with this program;
-// see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
-// <http://www.gnu.org/licenses/>.
-//
-// SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note
-//  arch/arm/include/asm/posix_types.h
-//
-//  Copyright (C) 1996-1998 Russell King.
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License version 2 as
-// published by the Free Software Foundation.
 import (
 	"os"
 	"time"
